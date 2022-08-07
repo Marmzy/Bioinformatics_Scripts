3eeda9a60b6511816a32cbb8694e8c83d9cb44b9
Marmzy <calvin.davey@gmail.com>
2022-08-07 10:22:53 +0900

    Added scripts for converting .bed6/.gtf files to .gff3 format

---
+++ conversion/bed6_to_gff3.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import pandas as pd
+
+from datetime import datetime
+
+
+def bed6_to_gff3(
+    input: str,
+    output: str
+) -> None:
+    """Convert .bed file to .gff3 file (modeled after R's rtracklayer)
+
+    Args:
+        input (str): Input .bed file path
+        output (str): Output .gff3 file path
+    """
+
+    #Reading the .bed file as a dataframe
+    bed_df = pd.read_csv(input, sep="\t", header=None, comment='#')
+
+    #Converting the .bed dataframe to a .gff3 dataframe
+    gff_df = pd.DataFrame({
+        "seqid": bed_df.iloc[:, 0],
+        "source": ["bed6_to_gff3"]*len(bed_df),
+        "type": ["sequence_feature"]*len(bed_df),
+        "start": bed_df.iloc[:, 1]+1,
+        "end": bed_df.iloc[:, 2],
+        "score": bed_df.iloc[:, 4],
+        "strand": bed_df.iloc[:, 5],
+        "phase": ["."]*len(bed_df),
+        "attributes": "name="+bed_df.iloc[:, 3]
+    })
+
+    #Outputting the gff3 dataframe
+    with open(output, "w") as f_out:
+        f_out.write("##gff-version 3\n")
+        f_out.write("##source-version ROSE\n")
+        f_out.write(f"##date {datetime.today().strftime('%Y-%m-%d')}\n")
+        gff_df.to_csv(f_out, sep="\t", header=False, index=False, mode="a")
---
+++ conversion/gtf_to_gff3.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import re
+
+from datetime import datetime
+
+
+def search(
+    attr: str,
+    entry: str
+) -> str:
+    """Checks if an attribute is present for a given .gtf entry
+
+    Args:
+        attr (str): .gtf attribute
+        entry (str): .gtf attributes entry
+
+    Returns:
+        str: Attribute=value concatenation
+    """
+
+    #Check if attribute is present for given entry, and if it is, return it's value
+    if re.search(f'{attr} "', entry):
+        return "{}={}".format(attr, re.findall(rf"(?<={attr} \")[^\"]+", entry)[0])
+
+
+def gtf_to_gff3(
+    input: str,
+    output: str
+) -> None:
+    """Convert .gtf file to .gff3 file (modeled after R's rtracklayer)
+
+    Args:
+        input (str): Input .bed file path
+        output (str): Output .gff3 file path
+    """
+
+    #Initialising variables
+    gtf_attributes = ["gene_id", "db_xref", "gbkey", "gene", "gene_biotype", "transcript_id",
+                      "model_evidence", "product", "exon_number", "protein_id", "anticodon",
+                      "inference", "note", "exception", "transl_except", "pseudo", "partial"]
+
+    #Reading the .gtf file as a dataframe
+    df = pd.read_csv(input, sep="\t", header=None, comment='#')
+    df.iloc[:, 1] = ["gtf_to_gff3"]*len(df)
+    df.iloc[:, 8] = [";".join([a for a in [search(attr, annot) for attr in gtf_attributes] if a is not None]) for annot in df.iloc[:, 8].values]
+
+    #Outputting the gff3 dataframe
+    with open(output, "w") as f_out:
+        f_out.write("##gff-version 3\n")
+        f_out.write("##source-version ROSE\n")
+        f_out.write(f"##date {datetime.today().strftime('%Y-%m-%d')}\n")
+        df.to_csv(f_out, sep="\t", header=False, index=False, mode="a")
